132d9661b11fdf5a341047ba13d1bdf061d6cde7
gray <gray@gmail.com>
2024-02-18 01:23:48 +0800

    fixed route config

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { ExercisesProvider } from './contexts/exercisesContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <BrowserRouter basename='/exerciseDemo'>
     <ExercisesProvider>
         <React.StrictMode>
           <App />
